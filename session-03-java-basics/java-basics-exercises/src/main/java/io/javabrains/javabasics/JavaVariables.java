457f93172760db7e3b4f454f97db02ba57151301
govi <govardhanreddy90@gmail.com>
2026-01-14 06:37:26 -0500

    java variables

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/JavaVariables.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/JavaVariables.java
@@ -4,29 +4,55 @@ public class JavaVariables {
 
     public static void main(String[] args) {
         // Step 1: Declare and initialize variables of different data types
+
+        int i;
+        int j, k;
+        long num;
+        double price;
+        double val, price2;
+        boolean isCondition;
+        char x;
+        float f;
+        String name;
         // int variable to store an integer value
-
+        i = 10;
+        j = 20_000;
+        k = 100;
         // long variable to store a long value
-
+        num = 100231;
         // float variable to store a floating-point value
-
+        f= 2.5f;
 
         // double variable to store a double value
-
-
+        price = 20.75;
+        val = 5.012E2;
+        price2 = 25D;
         // char variable to store a single character
-
-
+        x= 'x';
         // boolean variable to store a boolean value
+        isCondition = true;
 
+
+        var num1 = 10;
+        var str = "string";
 
         // String variable to store a string of characters
+        name = "gmr";
 
 
         // Step 2: Print the values of the variables to the console
+        System.out.println("i="+i);
+        System.out.println("j="+j);
+        System.out.println("k="+k);
+        System.out.println("num="+num);
+        System.out.println("price="+price);
+        System.out.println("price2="+price2);
+        System.out.println("val="+val);
+        System.out.println("isCondition="+isCondition);
+        System.out.println("str="+str);
+        System.out.println("x="+x);
 
-
-        // Step 3: Modify the values of the variables and print the updated values
+//         Step 3: Modify the values of the variables and print the updated values
 
     }
 }
